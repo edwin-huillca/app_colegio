66ad990ac4fe2d6487d4739a3f46c6959c5a76e7
Edwin Huillca <edwin@smartclickla.com>
2015-08-03 23:51:44 -0500

    Fixeando tarea 22

--- js/page_cl_login.js
+++ js/page_cl_login.js
@@ -1,6 +1,7 @@
 $( document ).on( 'pageinit', '#cl_login', function() {
 
   $('#cl_boton_submit').bind( "click", function(event, ui) {
+	if (cl_has_internet()){
 	
     $.mobile.loading( 'show', {
       text: 'Validando',
@@ -8,7 +9,7 @@ $( document ).on( 'pageinit', '#cl_login', function() {
       html: ''
     });
 	
-    if (cl_has_internet()){
+    
       user_login( $('#cl_usuario').val(), $('#cl_password').val(), {
       success:function(result){
         cl_variable_set( 'cl_user' , Drupal.user );
